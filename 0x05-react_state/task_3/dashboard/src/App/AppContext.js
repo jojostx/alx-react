93a00546be6420a66f2e42e19aeba6d420cb8af0
jojostx <ikuskid7@gmail.com>
2023-08-14 09:05:21 +0100

    solutions

---
+++ 0x05-react_state/task_3/dashboard/src/App/AppContext.js
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+export const user = {
+  email: "",
+  password: "",
+  isLoggedIn: false,
+};
+
+export const logOut = () => {
+  user.isLoggedIn = false;
+};
+
+export const AppContext = createContext({
+  user,
+  logOut
+});
